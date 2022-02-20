e96eed9beeb45b4af15d932147b55e7cefde480d
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2022-02-20 00:59:26 -0300

    fix(useRepositories):removing unusing state

--- src/services/requests/useFetchRepositories.ts
+++ src/services/requests/useFetchRepositories.ts
@@ -1,8 +1,6 @@
-import { useState } from "react"
 import { Repository } from "../../types/Repository"
 import { useFetch } from "./useFetch";
 
 export const useFetchRepositories = (user:string) => {
-    const [repositories, setRepositories] = useState<Repository[] | undefined>([]);
     return useFetch<Repository[]>(`users/${user}/repos`);
 }
